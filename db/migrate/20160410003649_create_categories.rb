d826a2e10015b2c795b6e7ed65d7ec145db5baa8
Benjamin Kemp <insightbck@gmail.com>
2016-04-10 00:37:19 +0000

    scaffold for category w/ casein generator

---
+++ db/migrate/20160410003649_create_categories.rb
@@ -0,0 +1,18 @@
+class CreateCategories < ActiveRecord::Migration
+  def self.up
+    create_table :categories do |t|
+      t.string :name
+      t.integer :category_id
+      t.string :description
+      t.string :permalink
+      t.datetime :created
+      t.integer :update_id
+      
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :categories
+  end
+end
